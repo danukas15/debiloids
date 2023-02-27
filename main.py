47bf312e50b46cfab04baf834c0ed673f55930e4
danukas15 <danukas76@gmail.com>
2023-02-27 22:16:25 +0200

    idomiai tu cia

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("ne")
-print("adios")
-#ciulpk
-print("as")
+
+breakfast = ["Kiausiniene", "Kiausinis", "Avokadas", "Bananas", "Jogurtas", "Sausi pusryciai", "Blynai"]
+snack = ["Riesutai", "Jogurtas", "Obuolys", "Spragesiai", "Avokadas", "Uogos/Vaisius", "Morka", "Kiausinis", "Krekeriai", "Pupeles", "Dziovinti vaisiai", ]
+Lunch = ["Salotos", "sriuba", "Rudi ryziai", ]
+
+

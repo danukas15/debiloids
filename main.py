fa518ee90284ed991926617c7c21c31cd3bba208
danukas15 <danukas76@gmail.com>
2023-02-27 22:27:55 +0200

    prirasiau komentaru cia vat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-
 breakfast = ["Kiausiniene", "Kiausinis", "Avokadas", "Bananas", "Jogurtas", "Sausi pusryciai", "Blynai"]
-snack = ["Riesutai", "Jogurtas", "Obuolys", "Spragesiai", "Avokadas", "Uogos/Vaisius", "Morka", "Kiausinis", "Krekeriai", "Pupeles", "Dziovinti vaisiai", ]
-Lunch = ["Salotos", "sriuba", "Rudi ryziai", ]
+snack = ["Riesutai", "Jogurtas", "Obuolys", "Spragesiai", "Avokadas", "Uogos/Vaisius", "Morka", "Kiausinis", "Krekeriai", "Pupeles", "Dziovinti vaisiai"]
+Lunch = ["Salotos", "sriuba", "Rudi ryziai"]
 
-
+# Randomizeris, isrenka is visu galimu viena, pvz is breakfast gauna kiausiniene, (Dar butu smagu kad tam tikri turetu didesni sansa)
+# atrinkimas, kad nebutu tas pats patiekalas per breakfast ir snack ir t.t.
+# Dar bus directory.
+Kiek_Valgyt = {
+    "kiausiniene": 3,
+}
